cf62967ae7c07c82e610704c31ad6be83ac64818
Lucasoftware7 <94875496+Lucasoftware7@users.noreply.github.com>
2022-12-20 00:46:42 -0300

    First and second exercises
    Solution to the first two exercises of the C# course

---
+++ Exercises1&2.cs
@@ -0,0 +1,11 @@
+
+
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Ingrese su nombre");
+string nombre;
+nombre=Console.ReadLine();
+Console.WriteLine("su nombre es {0}",nombre);
+
+Console.WriteLine("Ejercicio 2");
+DateTime tiempoActual= DateTime.Now;
+Console.WriteLine("La hora actual es: {0}",tiempoActual);
